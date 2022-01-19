92ff4cfeb709c6cba8baf0832abc03c03442611a
unknown <yisehakmitiku@gmail.com>
2022-01-19 16:30:07 +0800

    Create table medical history

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -3,3 +3,10 @@ CREATE TABLE patients (
     name VARCHAR(220),
     date_of_birth DATE
 );
+CREATE TABLE medical_histories (
+    id SERIAL PRIMARY KEY,
+    admitted_at TIMESTAMP,
+   patient_id INT,
+   status VARCHAR(220),
+   CONSTRAINT fk_patient FOREIGN KEY(patient_id) REFERENCES patients(id)  
+);
